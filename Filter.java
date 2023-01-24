0b3df191f180794abd4ec760ff34c35ce55fbc3d
stephane boguhe <boguhestephane97@gmail.com>
2023-01-24 18:47:51 +0100

    add interface to avoid duplication

---
+++ Filter.java
@@ -0,0 +1,14 @@
+
+/**
+ * Cette interface dispose d'une méthode.
+ * La méthode filter permettra d'éviter
+ * une duplication du code.
+ * 
+ * @author Boguhe Ange Stephane 
+ * @version 1.01
+ */
+public interface Filter {
+    
+    public boolean satisfie(QuakeEntry qe);
+
+}
